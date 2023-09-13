5479b01846e9b22b5d3080ab80af9fcb0e784f77
Siddeshwar <siddeshwar.work@gmail.com>
2023-09-13 21:07:21 +0530

    scrape updates code

--- backend/src/updates/updates.controller.ts
+++
@@ -1,9 +0,0 @@
-import { Controller, Post, Get, UseGuards, Request, Param } from '@nestjs/common';
-
-import { UpdatesService } from './updates.service'
-import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
-
-@Controller('review')
-export class UpdatesController {
-    constructor(private readonly updatesService: UpdatesService) { }
-}
