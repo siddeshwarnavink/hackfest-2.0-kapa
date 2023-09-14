385efc0edca6d8f02de5190724f8b5e359801e21
Siddeshwar <siddeshwar.work@gmail.com>
2023-09-14 08:15:11 +0530

    added button to map

--- frontend/components/community/MapComponent.tsx
+++ frontend/components/community/MapComponent.tsx
@@ -1,6 +1,39 @@
-import { useEffect } from 'react';
+import { useContext, useEffect } from 'react';
 import L from 'leaflet';
 import 'leaflet/dist/leaflet.css'; // Import Leaflet CSS
+import ReactDOMServer from 'react-dom/server';
+import { Button } from '@mantine/core';
+import { useQueryClient } from '@tanstack/react-query';
+import communityContext from '@/context/community';
+import { notifications } from '@mantine/notifications';
+
+function SwitchCommunityButton(props: { name: string }) {
+  // const queryClient = useQueryClient();
+  const communityCtx = useContext(communityContext);
+
+  const switchCommunity = (community: any) => {
+    // queryClient.invalidateQueries({ queryKey: ['homeFeed', communityCtx.community] });
+    communityCtx.setCommunity(community.id);
+    communityCtx.setCommunityName(community.name);
+    console.log(`Switching to ${community.name} ${community.id}`);
+    notifications.show({
+      title: 'Community',
+      message: `Currently on "${community.name}" community`,
+    });
+  };
+
+  return (
+    <div>
+      <Button
+        color='blue'
+        id={`switch-${props.name.replace(' ', '-')}`}
+        onClick={() => switchCommunity(props.name === 'SRM Vadapalani' ? { id: 'dffc1370-5247-11ee-ac55-ac1203516bd9', name: 'SRM Vadapalani' } : { id: 'dffc0b77-5247-11ee-ac55-ac1203516bd9', name: 'Koyambedu' })}
+      >
+        Switch
+      </Button>
+    </div>
+  );
+}
 
 class LocationMarker {
   name: string;
@@ -17,28 +50,23 @@ class LocationMarker {
 
   createMarker() {
 
-    // Create the marker and set the custom icon
-    // this.marker = L.marker(this.coordinates).addTo(this.map);
     this.marker = L.marker(this.coordinates, {
       icon: L.icon({
-        // iconUrl: process.env.NEXT_PUBLIC_LEAFLET_MAP_ICON, // Use the environment variable
         iconUrl: '/marker-icon.png',
         iconSize: [25, 41],
         iconAnchor: [12, 41],
         popupAnchor: [1, -34],
       }),
     }).addTo(this.map);
-
-    // Bind a popup to the marker
-    this.marker.bindPopup(`This is the ${this.name} marker.`);
-
+    const buttonHTML = ReactDOMServer.renderToString(
+      <SwitchCommunityButton name={this.name} />
+    );
+    this.marker.bindPopup(`This is the ${this.name} community. ${buttonHTML}`); // i want to add a <Button /> here
     let label = L.divIcon({ className: 'custom-label', html: this.name });
 
     L.marker([this.coordinates[0] - 0.001, this.coordinates[1]], { icon: label }).addTo(this.map);
-    // Handle marker click to zoom to the location
-
     this.marker.on('click', () => {
-      this.map.setView(this.coordinates, 13, { animate: true, duration: 3 }); // Adjust the zoom level and options as needed
+      this.map.setView(this.coordinates, 13, { animate: true, duration: 3 });
     });
   }
 
@@ -51,33 +79,27 @@ class LocationMarker {
 
 function Map() {
   useEffect(() => {
-    // Initialize the map
-    const map = L.map('map').setView([13.0501, 80.2110], 13); // Set initial coordinates and zoom level
+    const map = L.map('map').setView([13.0501, 80.2110], 13);
 
-    // Add a tile layer (you can use different map providers)
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
       attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(map);
 
     const locationName = new LocationMarker("SRM Vadapalani", [13.0501, 80.2110], map);
     const locationName2 = new LocationMarker("Koyambedu", [13.09, 80.0], map);
-    // const loc3 = new LocationMarker("Bay Of Bengal", [13.01, 80.5], map);
 
     locationName.createMarker();
     locationName2.createMarker();
-    // loc3.createMarker();
 
-    // Cleanup when the component unmounts
     return () => {
-      map.remove(); // Remove the map when the component unmounts
-      // loc3.removeMarker();
+      map.remove();
       locationName.removeMarker();
       locationName2.removeMarker();
     };
   }, []);
 
   return (
-  <div id="map" className="map-container"></div>
+    <div id="map" className="map-container"></div>
   );
 }
 
